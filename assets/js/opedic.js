20297863f2492a248818aef387aa3508830cacac
aimscare <aimscare@gmail.com>
2018-04-21 14:17:31 +0800

    Initial Commit
    abiraami hospital website

---
+++ assets/js/opedic.js
@@ -0,0 +1,61 @@
+jQuery(document).ready(function( $ ) {
+
+    // Initiate the wowjs animation library
+    new WOW().init();
+
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+
+
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".team-carousel").owlCarousel({
+        autoplay: false,
+        dots: true,
+        loop: true,
+        items: 4
+    });
+
+
+});
+
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 54)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 54
+    });
+
+
+
+})(jQuery); // End of use strict
+
+
+
+
